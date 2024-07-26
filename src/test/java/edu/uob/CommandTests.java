b27ce50f0ecbd15bdcaa7f359f34cf47090f8859
Zi <wp22629@bristol.ac.uk>
2024-07-26 14:17:11 +0100

    Added some more asserts to previous tests and made 'drop' test functional

--- src/test/java/edu/uob/CommandTests.java
+++ src/test/java/edu/uob/CommandTests.java
@@ -57,6 +57,10 @@ final class CommandTests {
     assertTrue(response5.contains("No such artefact here. Are you sure you spelled it correctly?"), "Incorrect or no response to Daniel: get testChara1");
     assertTrue(testLoc1.getCharacters().contains(testChar1), "Character incorrectly removed from location after 'get'");
     assertFalse(testPlayer.listArtefacts().contains("testChara1"), "Player inventory updates after 'get' despite character input");
+
+    // No artefact given:
+    String response6 = server.handleCommand("Daniel: get");
+    assertTrue(response6.contains("The command format seems off. Please use the correct format. Example: 'get sword', 'goto forest'."));
   }
 
   @Test
@@ -86,27 +90,39 @@ final class CommandTests {
     assertTrue(response4.contains("You moved to "), "Incorrect or no response to Daniel: goto testLoc2");
     assertTrue(response4.contains("testLoc2"), "Incorrect location in response to Daniel: goto testLoc2");
     assertEquals(testPlayer.getLocation(), testLoc2, "Player location does not change after 'goto'");
+
+    // No location specified:
+    String response5 = server.handleCommand("Daniel: goto");
+    assertTrue(response5.contains("The command format seems off. Please use the correct format. Example: 'get sword', 'goto forest'."));
   }
-/*
+
   @Test
-  void testDropCommand(){
-    // Set up test objects
-    Location testLoc1 = new Location("Location1", "Test location");
-    Player testPlayer = new Player(testLoc1);
-    Artefact testArte = new Artefact("Artefact1", "Test artefact");
-    testPlayer.addArtefact(testArte);
+  void testDropCommand() throws FileNotFoundException, ParseException {
+    // Setup:
+    Location testLoc1 = server.getLocationByName("testLoc1");
+    Player testPlayer = server.getPlayer();
+    Artefact testArte4 = new Artefact("testArte4", "Test artefact");
+    testPlayer.addArtefact(testArte4);
 
     // Valid input:
-    String response1 = server.handleCommand("Daniel: drop Artefact1");
-    assertTrue(response1.contains("You dropped"), "Incorrect or no response to Daniel: drop Artefact1");
-    assertFalse(testPlayer.listArtefacts().contains("Artefact1"), "Inventory still contains artefact after 'drop'");
-    assertTrue(testLoc1.getArtefacts().contains(testArte), "Location does not contain artefact after 'drop'");
+    String response1 = server.handleCommand("Daniel: drop testArte4");
+    assertTrue(response1.contains("You dropped the testArte4"), "Incorrect or no response to Daniel: drop testArte4");
+    assertFalse(testPlayer.listArtefacts().contains("testArte4"), "Inventory still contains artefact after 'drop'");
+    assertTrue(testLoc1.getArtefacts().contains(testArte4), "Location does not contain artefact after 'drop'");
 
-    // No artefact in inventory:
-    String response2 = server.handleCommand("Daniel: drop PhilosophersStone");
-    assertTrue(response2.contains("No such artefact"), "Incorrect or no response to Daniel: drop PhilosophersStone");
+    // Artefact not in inventory
+    String response2 = server.handleCommand("Daniel: drop testArte4");
+    assertTrue(response2.contains("You don't have such an artefact. Double-check your inventory."));
+
+    // Invalid artefact input:
+    String response3 = server.handleCommand("Daniel: drop PhilosophersStone");
+    assertTrue(response3.contains("You don't have such an artefact. Double-check your inventory."), "Incorrect or no response to Daniel: drop PhilosophersStone");
+
+    // No artefact given:
+    String response4 = server.handleCommand("Daniel: drop");
+    assertTrue(response4.contains("The command format seems off. Please use the correct format. Example: 'get sword', 'goto forest'."));
   }
-  */
+
   @Test
   void testResetCommand(){
       /* TO ADD:
